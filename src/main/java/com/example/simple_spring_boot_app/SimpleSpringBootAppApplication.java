b6a55762b6bc4722b34d1e6eaf524209854eb99b
AdrianRuchala <adrianruchala03@gmail.com>
2025-04-24 11:31:50 +0200

    Project Setup

---
+++ src/main/java/com/example/simple_spring_boot_app/SimpleSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.simple_spring_boot_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSpringBootAppApplication.class, args);
+	}
+
+}
